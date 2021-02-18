eba9d9d2f51fddd87c6feab2f4cc0d634e48f765
Sashin-Amichand <sashin.amichand@my.bdsc.school.nz>
2021-02-18 15:14:18 +1300

    3rd Version choices done

--- todo_list.py
+++ todo_list.py
@@ -7,11 +7,28 @@ routine = "1: Add a task \n2: View List \n3: Exit"
 
 # Asks the user what they want to do
 def choices(name):
-    print("Choose a mode to enter {}.\n{}".format(name, routine))
+    while True:
+        modeChoice = int(input("Enter a number to choose what to do {}.\n{}\n".format(name, routine)))
+        try:
+            if modeChoice == 1:
+                addingTask()
 
-
+            elif modeChoice == 2:
+                print(todo_list)
             
+            elif modeChoice == 3:
+                sys.exit()
+
+            else:
+                print("Enter only 1, 2 or 3")
+
+        except ValueError:
+            print("Please enter a valid number")
+                
 # The loop to add stuff to the to do list
+def addingTask():
+    taskadd = input("Please enter things you need to add to the to do list. \n")
+    todo_list
 
 # What to run when the user wants to exit
 
