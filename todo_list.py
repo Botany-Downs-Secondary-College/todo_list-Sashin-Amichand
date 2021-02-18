0eb66cdeeda30679ff08367bca08cb1e9b606922
Sashin-Amichand <sashin.amichand@my.bdsc.school.nz>
2021-02-18 14:30:19 +1300

    Structure V1

---
+++ todo_list.py
@@ -0,0 +1,17 @@
+# Imports system module to quit the program
+import sys
+
+# The to do list 
+todo_list = []
+# Asks the user what they want to do
+
+            
+# The loop to add stuff to the to do list
+
+# What to run when the user wants to exit
+
+# It will print the to do list
+
+
+# Calls the choice
+
